97814104389e853efa3ee86bb6847f451d595295
Batyr <bshekish@gmail.com>
2025-04-23 22:42:11 +0500

    Apply minor changes

--- lib/presentation/models/warehouse.dart
+++ lib/presentation/models/warehouse.dart
@@ -2,12 +2,10 @@ class Warehouse {
   final String id;
   final String name;
   final String imageUrl;
-  final String? description;
 
   Warehouse({
     required this.id,
     required this.name,
     required this.imageUrl,
-    this.description,
   });
 }
